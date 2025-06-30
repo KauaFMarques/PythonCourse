a6dcf50a4ea88b7c0cb36cd275b9a57e8c3d4b5c
KauaFMarques <kauamarques52@outlook.com>
2025-06-30 17:43:02 -0300

    praticando formatação de string na hora da impressão

---
+++ secao-11PythonBasico/aula01/aula56.py
@@ -0,0 +1,10 @@
+nome="kauã"
+sobrenome="ferreira"
+profissao="programador"
+
+texto="O "+nome+" "+sobrenome+" "+"é um excelente "+profissao
+print(texto)
+
+#melhor pratica
+texto2=f"O {nome} {sobrenome} é um excelente {profissao}"
+print(texto2)
