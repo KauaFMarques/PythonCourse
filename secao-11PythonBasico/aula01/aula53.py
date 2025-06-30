b52ea0a7881ebdf8f881edaea5492e644718d170
KauaFMarques <kauamarques52@outlook.com>
2025-06-30 17:25:29 -0300

    praticando input

---
+++ secao-11PythonBasico/aula01/aula53.py
@@ -0,0 +1,6 @@
+#o kaua tem 21 anos e mora em joão pessoa
+nome=input("Qual o seu nome:")
+idade=input("Qual o sua idade:")
+cidade=input("Qual o seu cidade:")
+
+print("O "+nome+" tem "+str(idade)+" emora em "+cidade)
