6f2ad31ec9336514b139d6792fd3ca8818289aee
KauaFMarques <kauamarques52@outlook.com>
2025-06-30 17:52:04 -0300

    praticando metodos em string

---
+++ secao-11PythonBasico/aula01/aula57.py
@@ -0,0 +1,7 @@
+mensagem="eEu Adoro Jogar"
+
+print(mensagem.lower())
+print(mensagem.upper())
+print(mensagem.capitalize())
+print(mensagem.find("Jogar"))
+print(mensagem.replace('Jogar','dormir'))
