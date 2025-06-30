afa397a1dae27d7dab164ffef9b86b1d678cc2aa
KauaFMarques <kauamarques52@outlook.com>
2025-06-30 17:37:48 -0300

    praticando dslice, pegando partes da variável usada e manipulando-a

---
+++ secao-11PythonBasico/aula01/aula55.py
@@ -0,0 +1,13 @@
+fruta="abacate"
+#      0123456
+print(fruta[0])
+print(fruta[1])
+print(fruta[2])
+print(fruta[3])
+print(fruta[4])
+print(fruta[5])
+print(fruta[6])
+print("-----------")
+print(fruta[0:4])
+#segundo as regras de python ele n pega o último elemento, mas sim o antepenultimo da lista
+print(fruta[-1])
