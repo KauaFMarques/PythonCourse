d67f75d4adcd8878b2829acd5bfce73c18ea51a1
KauaFMarques <kauamarques52@outlook.com>
2025-07-01 15:53:18 -0300

    aula 61,elif if else

---
+++ secao13-Controle de Fluxo/aula61.py
@@ -0,0 +1,10 @@
+velocidade=40
+
+if(velocidade>110):
+    print("velocidade limite atingida! reduza para 100")
+
+elif(velocidade<=40):
+    print("favor dirigir acima de 80km")
+
+else:
+    print("velocidade ok!")
