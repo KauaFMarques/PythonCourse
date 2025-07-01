4dd1aa45224c38864779125d192c05958d465cb0
KauaFMarques <kauamarques52@outlook.com>
2025-07-01 17:25:30 -0300

    aula63

---
+++ secao13-Controle de Fluxo/aula63.py
@@ -0,0 +1,12 @@
+valor=10
+
+if 20<=valor<40:
+    print("Aceito")
+else:
+    print("valor não aceito")
+
+'''
+observe que isso ficou mais limpo do que isso:
+if valor>=20 and valor<40:
+    pois escreve o atributo valor apenas uma única vez
+'''
