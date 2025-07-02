3e82bacc05112b9583501dd5ef7c114eeaafcd5b
KauaFMarques <kauamarques52@outlook.com>
2025-07-02 17:30:10 -0300

    aula 79 aprendendo argumentos em funções

---
+++ secao14-Funcoes/aula79.py
@@ -0,0 +1,5 @@
+def boas_vindas(nome,mensagem,quantidade):
+    print(f'Olá {nome}, desejo para vc:{mensagem}')
+    print(f'temos {str(quantidade)} de notebbooks disponíveis')
+
+boas_vindas("Kauã","tudo de bom na sua vida",5)
