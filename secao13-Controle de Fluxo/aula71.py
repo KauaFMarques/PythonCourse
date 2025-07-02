bfbd969ec2a8c61b86d1233114f0b16d38c70293
KauaFMarques <kauamarques52@outlook.com>
2025-07-02 15:29:45 -0300

    aula 71, operadores ternários

---
+++ secao13-Controle de Fluxo/aula71.py
@@ -0,0 +1,11 @@
+idade=14
+
+'''
+if idade>=16:
+    resultado=print("voto permitido")
+else:
+    resultado=print("voto não permitido")
+'''
+
+resultado='voto permitido' if idade>=16 else 'voto não permitivo'
+print(resultado)
