2f5cf038e66a089c6639c53d6ef4489df471d06d
KauaFMarques <kauamarques52@outlook.com>
2025-07-03 16:32:53 -0300

    aula 93, aprendendo a diferença entre listas e tuples

---
+++ secao15-Listas/aula93.py
@@ -0,0 +1,7 @@
+cors=['red','yellow','blue']
+cors_tuple=('red','yellow','blue')
+
+print(type(cors))
+print(type(cors_tuple))
+#impossivel alterar, remover e add nas tuples. Devemos usar tuples com lista de intens fixos
+#listas gastam mais espaços q tuple
