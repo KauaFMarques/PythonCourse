624fda33cc26ac3e80c347b0751e8c0dc4d12cc2
KauaFMarques <kauamarques52@outlook.com>
2025-07-03 15:41:02 -0300

    aula 86 adicionando funções em listas

---
+++ secao15-Listas/aula87.py
@@ -0,0 +1,13 @@
+numeros=[1,2,3,4]
+letras=['a','b','c','d']
+
+final=numeros+letras
+print(final)
+
+numeros.extend(letras)
+print(numeros)
+
+item=['i1','i2'],['i3','i4']
+print(item)
+print(item[1])
+print(item[1][0])
