abd21dcfd5de4ed8705b1145dcfe6c45e0f91a57
KauaFMarques <kauamarques52@outlook.com>
2025-07-03 16:15:23 -0300

    aula 89, loops em listas

---
+++ secao15-Listas/aula88.py
@@ -0,0 +1,4 @@
+produtos=['arroz','feijao','macarrao']
+
+item1=produtos[0]
+print(item1)
---
+++ secao15-Listas/aula89.py
@@ -0,0 +1,4 @@
+interacoes=[1,2,3]
+
+for tentativa in interacoes:
+    print(f'tentativa no dado:{tentativa}')
