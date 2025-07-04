e2e9e976842d6845f009eae0eb5cfdddba9ea288
KauaFMarques <kauamarques52@outlook.com>
2025-07-04 15:50:44 -0300

    aula 116, criando os primeiros objetos

---
+++ secao17-OPP/aula116.py
@@ -0,0 +1,14 @@
+#criar classe
+class Funcionarios:
+    pass
+#criar objeto
+usuario1=Funcionarios()
+
+#criar paramentros
+usuario1.nome='helena'
+usuario1.sobrenome='cabral'
+usuario1.dataNascimento='12/05/2003'
+
+print(usuario1.nome)
+print(usuario1.sobrenome)
+print(usuario1.dataNascimento)
