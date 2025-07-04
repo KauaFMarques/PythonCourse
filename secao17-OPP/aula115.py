61eda4e88f4b19c380a94a68a7a428778168b394
KauaFMarques <kauamarques52@outlook.com>
2025-07-04 15:47:06 -0300

    aula 115, vendo classes

---
+++ secao17-OPP/aula115.py
@@ -0,0 +1,9 @@
+class Funcionarios:
+    nome='Helena'
+    sobrenome='Cabral'
+    dataNascimento="12/05/2003"
+
+usuario1=Funcionarios()
+print(usuario1.nome)
+print(usuario1.sobrenome)
+print(usuario1.dataNascimento)
