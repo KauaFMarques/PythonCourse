14ee98f103777c1e2e6aa65b1a59e28392b99e36
KauaFMarques <kauamarques52@outlook.com>
2025-07-04 16:24:16 -0300

    aprendendo importação de funções

---
+++ secao18-Arquivos/arquivos1/funcoes.py
@@ -0,0 +1,8 @@
+def somar(a,b):
+    resultado=a+b
+    return print(resultado)
+
+
+def multi(a,b):
+    resultado=a*b
+    return print(resultado)
---
+++ secao18-Arquivos/arquivos1/main.py
@@ -0,0 +1,4 @@
+from funcoes import somar,multi
+
+test1=somar(2,3)
+test2=multi(3,4)
