bfc085653e84815b6b79590b667c36a996b8dba7
KauaFMarques <kauamarques52@outlook.com>
2025-07-04 16:12:41 -0300

    aprendendo métodos de retorno nas classes construtoras de objetos

--- secao17-OPP/aula117.py
+++ secao17-OPP/aula117.py
@@ -1,12 +1,21 @@
+from datetime import datetime
 #criar classe
 class Funcionarios:
-    def __init__(self,nome,sobrenome,data_nascimento):
+    def __init__(self,nome,sobrenome,ano_nascimento):
         self.nome=nome
         self.sobrenome=sobrenome
-        self.data_nascimento=data_nascimento
-#criar objeto
-usuario1=Funcionarios('helena','cabral','12/08/2004')
+        self.ano_nascimento=ano_nascimento
 
-print(usuario1.nome)
-print(usuario1.sobrenome)
-print(usuario1.data_nascimento)
+    def imprimirNomeSobrenome(self):
+        return self.nome+" "+self.sobrenome
+    
+    def calcula_Idade(self):
+        ano_atual=datetime.now().year
+        self.ano_nascimento=int(ano_atual-self.ano_nascimento)
+        return self.ano_nascimento
+#criar objeto
+usuario1=Funcionarios('helena','cabral',2003)
+
+print(usuario1.imprimirNomeSobrenome())
+print(Funcionarios.imprimirNomeSobrenome(usuario1))
+print(Funcionarios.calcula_Idade(usuario1))
